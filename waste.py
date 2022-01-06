8d1deb6767723272a2d579973ade371d9a284950
inv-Ayiba <tariogounga@gmail.com>
2022-01-06 19:10:01 +0100

    move

---
+++ waste.py
@@ -0,0 +1,23 @@
+Constructs
+Sequences
+Perceptual
+Reasoning
+Interpolation
+Deduction
+Analysis
+Dynamics
+Tracing
+Formation
+3D tests
+Reckoning
+Arithmetic
+Figure work
+Number skills
+Calculations
+Systems
+Coding
+Word usage
+Vocabulary
+Verbal precision
+Core intelligence test
+Dispositional aptitude test
