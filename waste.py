b0b6e32bc7f39bb7ebeab43f11f40996ebb9e0a2
inv-Ayiba <tariogounga@gmail.com>
2022-01-07 13:44:38 +0100

    please work

--- waste.py
+++ waste.py
@@ -1,23 +1,23 @@
-Constructs
-Sequences
-Perceptual
-Reasoning
-Interpolation
-Deduction
-Analysis
-Dynamics
-Tracing
-Formation
-3D tests
-Reckoning
-Arithmetic
-Figure work
-Number skills
-Calculations
-Systems
-Coding
-Word usage
-Vocabulary
-Verbal precision
-Core intelligence test
-Dispositional aptitude test
+"constructs.html"
+"sequences.html"
+"perceptual.html"
+"reasoning.html"
+"interpolation.html"
+"deduction.html"
+"analysis.html"
+"dynamics.html"
+"tracing.html"
+"formation.html"
+"3dtests.html"
+"reckoning.html"
+"arithmetic.html"
+"figurework.html"
+"numberskills.html"
+"calculations.html"
+"systems.html"
+"coding.html"
+"wordusage.html"
+"vocabulary.html"
+"verbalprecision.html"
+"coreintelligencetest.html"
+"dispositionalaptitudetest.html"
