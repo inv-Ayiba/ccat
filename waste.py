74d966e82c2fde434175ae45714f71cfd99858db
inv-Ayiba <tariogounga@gmail.com>
2022-09-07 15:48:26 +0100

    added some instructions

--- waste.py
+++ waste.py
@@ -1,23 +1,37 @@
-"constructs.html"
-"sequences.html"
-"perceptual.html"
-"reasoning.html"
-"interpolation.html"
-"deduction.html"
-"analysis.html"
-"dynamics.html"
-"tracing.html"
-"formation.html"
-"3dtests.html"
-"reckoning.html"
-"arithmetic.html"
-"figurework.html"
-"numberskills.html"
-"calculations.html"
-"systems.html"
-"coding.html"
-"wordusage.html"
-"vocabulary.html"
-"verbalprecision.html"
-"coreintelligencetest.html"
-"dispositionalaptitudetest.html"
+# "constructs.html"
+# "sequences.html"
+# "perceptual.html"
+# "reasoning.html"
+# "interpolation.html"
+# "deduction.html"
+# "analysis.html"
+# "dynamics.html"
+# "tracing.html"
+# "formation.html"
+# "3dtests.html"
+# "reckoning.html"
+# "arithmetic.html"
+# "figurework.html"
+# "numberskills.html"
+# "calculations.html"
+# "systems.html"
+# "coding.html"
+# "wordusage.html"
+# "vocabulary.html"
+# "verbalprecision.html"
+# "coreintelligencetest.html"
+# "dispositionalaptitudetest.html"
+
+# # dynamics
+# console.log(placesAns)
+me1=['C', 'C', 'C', 'C', 'D', 'C', 'A', 'C', 'B', 'C', 'A', 'B', 'B', 'A', 'B', 'B', 'C', 'B', 'F', 'B', 'D', 'D', 23, 24, 25]
+# undefined
+# console.log(answer)
+ans=['C', 'C', 'C', 'C', 'B', 'A', 'A', 'A', 'B', 'C', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'C', 'B', 'D', 'A', 'D', 'C', 'B']
+
+me2=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 'A', 'B', 'F', 'B', 'D', 'A', 'D', 'D', 'B']
+num0=21
+num=num0-1
+for i in range(25):
+    num=i
+    print(i+1," ans:",ans[num],"  me1",me1[num]," me2",me2[num])
